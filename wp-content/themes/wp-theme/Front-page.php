3b44d3a4f73a8798ea8a62f51e604c8ff3e4e249
jamanul <jk.skb.25@gmail.com>
2025-01-08 17:30:22 +0600

    created custom post'

--- wp-content/themes/wp-theme/Front-page.php
+++ wp-content/themes/wp-theme/Front-page.php
@@ -4,11 +4,18 @@
 <section id="service_area">
     <div class="container">
         <div class="row">
-            <div class="col-md-4">
-                <div class="service-content">
-
-                </div>
-            </div>
+            <?php query_posts('post_type=service&post_status=publish&posts_per_page=6&order=ASC&paged=' . get_query_var('post')); ?>
+            <?php if (have_posts()):
+                while (have_posts()):
+                    the_post(); ?>
+                    <div class="col-md-4">
+                        <div class="service-content">
+                            <h2><?php echo the_title() ?></h2>
+                            <?php echo the_post_thumbnail('service') ?>
+                            <?php echo the_excerpt() ?>
+                        </div>
+                    </div>
+                <?php endwhile; endif; ?>
         </div>
     </div>
 </section>
